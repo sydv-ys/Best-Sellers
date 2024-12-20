9193bde65d4abedd1f6a3f08e49b3e4c24c0bf50
sydv-ys <aysu.seyidova@kibrit.tech>
2024-12-20 16:47:48 +0400

    Numbering and clean-up

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Book.module.css";
 
-function Book({ img, title, author, getBook, id }) {
+function Book({ img, title, author, number }) {
+  console.log(number);
+
   return (
     <div className={styles.book}>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4> {author} </h4>
-      <button onClick={() => getBook(id)}>click me</button>
+      <span className={styles.number}>{`# ${number}`}</span>
     </div>
   );
 }
--- src/BookList.jsx
+++ src/BookList.jsx
@@ -1,46 +1,14 @@
 import React from "react";
 import Book from "./Book";
 import styles from "./BookList.module.css";
-import {books} from "./Books"
+import { books } from "./Books";
 
 function BookList() {
- 
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
-    const getBookName = (e, title) => {
-    if (e.target.value === title) {
-      console.log(title);
-    } else {
-      console.log("not a match");
-    }
-  };
-  const Event = () => {
-    const handleFormInput = (e) => {};
-
-    const handleFormSubmittion = (e) => {
-      e.preventDefault();
-      console.log("form submitted");
-    };
-
-    return (
-      <>
-        <form onSubmit={handleFormSubmittion}>
-          <h1>form</h1>
-          <input type="text" name="example" onChange={handleFormInput} />
-          <button onClick={getBookName}>find</button>
-        </form>
-      </>
-    );
-  };
-
   return (
     <div className={styles.booklist}>
-      <Event />
-      {books.map((book) => {
+      {books.map((book, index) => {
         const { id } = book;
-        return <Book {...book} key={id} getBook={getBook} />;
+        return <Book {...book} key={id} number={index + 1} />;
       })}
     </div>
   );
